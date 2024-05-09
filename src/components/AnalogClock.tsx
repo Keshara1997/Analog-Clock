83473eb41d7a5c8cd814387442dfefcb099238e3
Keshara1997 <kavindakeshara97@gmail.com>
2024-05-09 15:43:32 +0530

    functionalty

--- src/components/AnalogClock.tsx
+++ src/components/AnalogClock.tsx
@@ -1,25 +1,106 @@
-import React, { FC } from 'react';
-
-interface AnalogClockProps {
-  showSecondHand: boolean;
-  showMiniuteHand: boolean;
-  showHourHand: boolean;
-  numbersType: 'numbers' | 'dots';
-}
-
+import React, { FC, ReactElement, useEffect, useState } from "react";
+import "./AnalogClock.css";
+import { AnalogClockProps } from "./analogClockTypes";
+import { getClockRotation } from "./utils";
 const AnalogClock: FC<AnalogClockProps> = ({
-
-  numbersType = 'numbers',
+  showMiniuteHand = true,
+  showSecondHand = true,
+  showBorder = true,
+  showHandBase = true,
+  smoothSeconds = false,
+  numbersType = "numbers",
+  borderColor = "#000000",
+  numbersColor = "#000000",
+  handBaseColor = "#000000",
+  handColor = { hour: "#000000", miniute: "#000000", second: "#e74c3c" },
+  handLength = { hour: "70px", miniute: "90px", second: "90px" },
+  size = "200px",
+  staticDate,
 }) => {
-  const numberElements: Array<string> = new Array(12)
-    .fill('')
-    .map((_, index) => (
-      <span className="analog-clock-numbers">
-        ${numbersType === 'numbers' ? index + 1 : '.'}
+  const [date, setDate] = useState(staticDate || new Date());
+
+  const rotations = getClockRotation(date);
+
+  useEffect(() => {
+    if (!staticDate) {
+      const intervalTime = showSecondHand
+        ? 1000
+        : showMiniuteHand
+        ? 1000 * 60
+        : 1000 * 3600;
+      const interval = setInterval(() => setDate(new Date()), intervalTime);
+      return () => clearInterval(interval);
+    }
+  }, [staticDate, showMiniuteHand, showSecondHand]);
+
+  const numberElements: ReactElement[] = [
+    <span
+      className="analog-clock-number"
+      id="analog-clock-number-12"
+      style={{ color: numbersColor }}
+    >
+      {numbersType === "numbers" ? 12 : "."}
+    </span>,
+    ...new Array(11).fill("").map((_, index) => (
+      <span
+        className="analog-clock-number"
+        id={`analog-clock-number-${index+1}`}
+        style={{ color: numbersColor }}
+      >
+        {numbersType === "numbers" ? index + 1 : "."}
       </span>
-    ));
+    )),
+  ];
 
-  return <div className="analog-clock"></div>;
+  return (
+    <div
+      className="analog-clock"
+      style={{
+        border: showBorder ? `2px solid ${borderColor}` : "none",
+        width: size,
+        height: size,
+      }}
+    >
+      {" "}
+      {showHandBase && (
+        <div className="analog-clock-handbase" style={{ color: handBaseColor }}>
+          ●
+        </div>
+      )}
+      {showSecondHand && (
+        <div
+          className="analog-clock-second"
+          style={{
+            backgroundColor: handColor.second,
+            width: handLength.second,
+            left: `50%`,
+            transform: `rotate(${rotations.seconds}deg)`,
+            transition: smoothSeconds ? "transform 0.3s ease" : "none",
+          }}
+        ></div>
+      )}
+      {showMiniuteHand && (
+        <div
+          className="analog-clock-minute"
+          style={{
+            backgroundColor: handColor.miniute,
+            width: handLength.miniute,
+            left: `50%`,
+            transform: `rotate(${rotations.minutes}deg)`,
+          }}
+        ></div>
+      )}
+      <div
+        className="analog-clock-hour"
+        style={{
+          backgroundColor: handColor.hour,
+          width: handLength.hour,
+          left: `50%`,
+          transform: `rotate(${rotations.hours}deg)`,
+        }}
+      ></div>
+      <div className="analog-clock-numbers"> {numberElements} </div>{" "}
+    </div>
+  );
 };
-
 export default AnalogClock;
